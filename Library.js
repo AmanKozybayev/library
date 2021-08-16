4d18d555e3c5d6d73c7aba77e4e84c192e508909
Aman Kozybayev <Enphas@gmail.com>
2021-08-16 16:30:07 +0500

    Udalit' knigu (splice)

--- Library.js
+++ Library.js
@@ -19,9 +19,21 @@ class Book {
       //create a method that will remove certain book from list of books
     removeBook(_title){
       //v dushe ne ebu kak eto sdelat' za odin shag ** vrode prishla ideya, no postoyannaya putanica v golove
-     
+      let b=_title;
+      for (let i=0; i<this.library.length;i++){
+        if (this.library[i].title===b){
+        this.library.splice(i,1);
+        break
+        }
+        else if (i!=this.library.length-1 && b!=this.library[i].title){
+          console.log('searching...');
+          }
+          else {
+            console.log('Your book not found!');
+            }
+        }
       }
-      }
+      
       //searching book by its title
     findBook(_title){
       //zdes' budet input, dopustim on budet nazivat'sya b
@@ -40,7 +52,8 @@ class Book {
         }
       
       }
-    }
+  }//kak zhe dostali eti skobki ska
+
   let myLib = new Library();
   myLib.addBook('Harry Potter','joeanne rowling',1997);
   console.log(myLib);
